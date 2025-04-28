8a372d2909c6915aa4e0da21d20cadd1e8a8464b
maheshbasnet089 <basnetmanish089@gmail.com>
2025-04-28 21:48:28 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express") // express require gareko
+const app = express() // express lai trigger gareko 
+require("./database/connection")
+
+// let app = require("express")()
+app.get("/books",function(req,res){
+    // logic to fetch books from database 
+    res.json({
+        message : "books fetched successfully"
+    })
+})
+
+app.post("/books",function(req,res){
+    // logic to add book to database goes here.. 
+
+    res.json({
+        message : "Book added successfully"
+    })
+})
+
+app.delete("/books/:id",function(req,res){
+    // logic to delete book
+    res.json({
+      message : "Book Deleted successfully"
+    })
+})
+app.patch("/books/:id",function(req,res){
+    // logic to update book
+    res.json({
+      message : "Book updated successfully"
+    })
+})
+
+postgresql://postgres.scfmicugsxklxgoyqgar:hahahehehuhu@aws-0-ap-south-1.pooler.supabase.com:6543/postgres
+
+app.listen(4000, function () {
+    console.log("project/server/backend has started at port 4000")
+})
---
+++ database/connection.js
@@ -0,0 +1,29 @@
+
+// hami yaha database connection ko code/logic lekxam
+const {Sequelize,DataTypes} =  require("sequelize")
+
+// const sequelize = require("sequelize")
+// const Sequelize = sequelize.Sequelize
+// const DataTypes = sequelize.DataTypes
+
+
+const sequelize = new Sequelize("postgresql://postgres.cnyaqklytzrovjdkjexp:hahahehehuhu1234@aws-0-ap-south-1.pooler.supabase.com:6543/postgres")
+
+
+sequelize.authenticate()
+.then(()=>{
+    console.log("Authenticated vayo, connected vayo !!")
+})
+.catch((err)=>{
+    console.log("Error aayo" + err)
+})
+
+const db  = {}
+db.Sequelize = Sequelize 
+db.sequelize = sequelize
+
+module.exports = db
+
+
+
+
