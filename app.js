8680cb5e93fc8409b51a0d550eb2e3247cddd1fd
maheshbasnet089 <basnetmanish089@gmail.com>
2025-04-30 21:12:55 +0545

    post book api

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { books } = require("./database/connection")
 
 const app = express() // express lai trigger gareko 
 require("./database/connection")
+app.use(express.json())
 //test
 // let app = require("express")()
 app.get("/books",async function(req,res){
@@ -15,9 +16,21 @@ app.get("/books",async function(req,res){
     })
 })
 
-app.post("/books",function(req,res){
+app.post("/books",async function(req,res){
     // logic to add book to database goes here.. 
+    // console.log(req.body)
+    // const bookName = req.body.bookName
+    // const bookPrice = req.body.bookPrice
 
+    const {bookName,bookPrice,bookAuthor,bookGenre} = req.body
+    // check if all data aako xa vane only proceed , else not proceed throw error in response
+    await books.create({
+        bookName,
+        price : bookPrice, 
+        bookAuthor, 
+        bookGenre
+         // columName : value
+    })
     res.json({
         message : "Book added successfully"
     })
